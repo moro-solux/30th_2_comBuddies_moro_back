1cae3c3367504011999e84338fa89abca64fabab
이수 <149984963+suuu004@users.noreply.github.com>
2025-12-21 16:19:47 +0900

    테이블 관계 수정

--- src/main/java/com/example/moro/app/mission/Mission.java
+++ src/main/java/com/example/moro/app/mission/Mission.java
@@ -1,12 +1,11 @@
 package com.example.moro.app.mission;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -21,4 +20,9 @@ public class Mission {
     private String missionTitle;   // 미션 제목
 
     private LocalDateTime createdAt;   // 미션 생성 시간
+
+    // 양방향 설정: 하나의 미션은 여러 포스트를 가질 수 있음 (1:N)
+    // 미션 객체에 해당하는 모든 포스트 목록 보고 싶을 떄를 위해 만듦.
+    @OneToMany(mappedBy = "mission")
+    private List<MissionPost> missionPosts = new ArrayList<>();
 }
--- src/main/java/com/example/moro/app/mission/MissionPost.java
+++ src/main/java/com/example/moro/app/mission/MissionPost.java
@@ -16,12 +16,15 @@ public class MissionPost {
     @GeneratedValue
     private Long misPostId;  // 미션 게시물 id
 
-    // Mission 테이블과 관계(M:1)
+    // Mission 테이블과 연결: 여러 개의 포스트는 하나의 미션에 속함 (N:1)
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name= "mission_id")
+    @JoinColumn(name= "mission_id")  // DB의 외래키 컬럼명과 매칭
     private Mission mission;
 
-    private Long userId;   // 사용자 아이디
+    // User 테이블과 관계 설정 필요함
+    //@ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name = "user_id")
+    //private Long userId;   // 사용자 아이디
 
     private LocalDateTime createdAt; // 생성일
     private String imageUrl;   // 이미지
