8662ded59595bf9ab7465cd254120d574fac96b1
이수 <149984963+suuu004@users.noreply.github.com>
2025-12-31 12:20:17 +0900

    컬러맵 조회 service

---
+++ src/main/java/com/example/moro/app/colormap/service/ColorMapService.java
@@ -0,0 +1,41 @@
+package com.example.moro.app.colormap.service;
+
+import com.example.moro.app.colormap.dto.ColorDetailResponse;
+import com.example.moro.app.colormap.dto.ThemeGroupResponse;
+import com.example.moro.app.colormap.entity.UserColorMap;
+import com.example.moro.app.colormap.repository.UserColorMapRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ColorMapService {
+    private final UserColorMapRepository userColorMapRepository;
+
+    public List<ThemeGroupResponse> getUserColorMapList(Long memberId) {
+        // 모든 색상 정보와 사용자의 해금 상태 한 번에 조회
+        List<UserColorMap> userColors = userColorMapRepository.findAllByMemberIdWithColorMap(memberId);
+
+        // 테마 별로 그룹화해 변환
+        return userColors.stream()
+                .collect(Collectors.groupingBy(ucm -> ucm.getColorMap().getColorTheme()))
+                .entrySet().stream()
+                .map(entry -> new ThemeGroupResponse(
+                        entry.getKey(),
+                        entry.getValue().stream()
+                                .map(ucm -> new ColorDetailResponse(
+                                        ucm.getColorMap().getColorId(),
+                                        ucm.getColorMap().getHexCode(),
+                                        ucm.getPostCount(),
+                                        ucm.getUnlocked(),
+                                        ucm.getIsRepresentative()
+                                ))
+                                .collect(Collectors.toList())
+                ))
+                .collect(Collectors.toList());
+    }
+}
