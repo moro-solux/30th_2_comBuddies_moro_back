e9289dfcdfbde5d69511bfe153a9dd995409bc77
SangHee-Hong <hongdabagi@gmail.com>
2026-01-02 01:50:15 +0900

    feat: google maps api 연동

---
+++ src/main/java/com/example/moro/global/config/GoogleMapsConfig.java
@@ -0,0 +1,21 @@
+package com.example.moro.global.config;
+
+
+import com.google.maps.GeoApiContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GoogleMapsConfig {
+
+    @Value("${spring.google.maps.api.key}")
+    private String apiKey;
+
+    @Bean
+    public GeoApiContext geoApiContext() {
+        return new GeoApiContext.Builder()
+                .apiKey(apiKey)
+                .build();
+    }
+}
