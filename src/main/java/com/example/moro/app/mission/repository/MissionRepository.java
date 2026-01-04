9d62e7674acdaec1ab64b5625962b7e7cc65bd5a
SangHee-Hong <hongdabagi@gmail.com>
2026-01-04 23:07:30 +0900

    feat: 미션 마감 1시간 전 알림 서비스 연결

--- src/main/java/com/example/moro/app/mission/repository/MissionRepository.java
+++ src/main/java/com/example/moro/app/mission/repository/MissionRepository.java
@@ -2,11 +2,21 @@ package com.example.moro.app.mission.repository;
 
 import com.example.moro.app.mission.entity.Mission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MissionRepository extends JpaRepository<Mission,Long> {
     // 미션 조회
     Optional<Mission> findFirstByOrderByCreatedAtDesc();
+
+    @Query(value = "SELECT * FROM mission m " +
+            "WHERE DATE_ADD(m.created_at, INTERVAL 10 HOUR) <= NOW() " +
+            "AND DATE_ADD(m.created_at, INTERVAL 12 HOUR) > NOW()",
+            nativeQuery = true)
+    List<Mission> findMissionsEndingInOneHour();
+
 }
---
+++ src/main/java/com/example/moro/app/mission/service/MissionNotificationScheduler.java
@@ -0,0 +1,40 @@
+package com.example.moro.app.mission.service;
+
+import com.example.moro.app.member.entity.Member;
+import com.example.moro.app.member.repository.MemberRepository;
+import com.example.moro.app.mission.entity.Mission;
+import com.example.moro.app.mission.repository.MissionRepository;
+import com.example.moro.app.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MissionNotificationScheduler {
+
+    private final NotificationService notificationService;
+    private final MemberRepository memberRepository;
+    private final MissionRepository missionRepository;
+
+    //@Scheduled(cron = "*/1 * * * * *") // 매 1초마다 실행
+    @Scheduled(cron = "0 0 11,23 * * *") // 오전 11시와 오후 11시에만 실행
+    public void sendMissionEndNotifications() {
+        List<Mission> missions = missionRepository.findMissionsEndingInOneHour();
+
+        for (Mission mission : missions) {
+            List<Member> targetMembers = memberRepository.findNonParticipants(mission);
+
+            targetMembers.forEach(member ->
+                    notificationService.notifyMission(
+                            member.getId(),
+                            mission.getMissionId(),
+                            mission.getMissionTitle(),
+                            1
+                    )
+            );
+        }
+    }
+}
