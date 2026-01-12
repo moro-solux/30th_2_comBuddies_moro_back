6014a62f66e4dc418eb6885833f6913c28a26601
SangHee-Hong <hongdabagi@gmail.com>
2026-01-13 00:55:05 +0900

    feat: 한영 주소 반환 dto 작성

---
+++ src/main/java/com/example/moro/app/map/dto/GetAddressResponse.java
@@ -0,0 +1,11 @@
+package com.example.moro.app.map.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GetAddressResponse {
+    private String addressKo;
+    private String addressEn;
+}
